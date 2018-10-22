e6693cdb47621a9a53de599d14178244fcb632d6
pratapj <pratapjayaram13@gmail.com>
2018-10-21 20:26:47 -0500

    Next Project!
    Added next project button to appropriate pages

--- projects.js
+++ projects.js
@@ -16,3 +16,13 @@ $(".home").click(function(){
 
 var projects = ["labelinsight.html", "uptake.html", "biteback.html", "spongee.html", "mockups.html"];
 var current = 0;
+
+$(".next-project").click(function(){
+    var project = window.location.href;
+    for (var i = 0; i < project.length; i++){
+        if (project.includes(projects[i])) current = i;
+    }
+    console.log(window.location.href);
+    // window.location.href = "www.pratapjayaram.com/" + projects[current+1];
+    window.location.href = "file:///C:/Users/prata/Documents/GitHub/portfolio/"
+ + projects [current+1]});
